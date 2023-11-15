a743ff3734b72275e8dfb507d3f2acf8bc3faf5b
Maggie-Njeri <mkiri00@gmail.com>
2023-11-15 11:59:10 -0800

    simple shell

---
+++ scrivo.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+/**
+ * mc_fprint is used to print out the results
+ * scrivo: is a pointer to store the results
+ */
+
+void mc_fprint(const char *scrivo) 
+{
+        write(STDOUT_FILENO, scrivo, strlen(scrivo));
+}
---
+++ shell.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/*
+ * main - Entry point of the simple shell program.
+ * Description: function serves as the main entry point for the simple shell.
+ * It enters into loop, displays prompt, reads user input,& executes commands.
+ * @domande: is the string to be worked
+ * Return: Always 0 (success).
+ */
+
+int main(void)
+{
+	char domande[120];
+
+	while (true)
+	{
+		display_prompt();
+		leggere_domande(domande, sizeof(domande));
+		execute_domande(domande);
+
+	}
+
+	return (0);
+}
